7db34a02c4c9ed5b568e565b71f9b257750244a3
Nima Safabakhsh <nima.safabakhsh@gmail.com>
2021-02-22 17:38:34 -0500

    Set the inputs for color, row, and columns

--- designs.js
+++ designs.js
@@ -1,5 +1,9 @@
 // Select color input
+let color_picked = document.getElementById('colorPicker').value;
+
 // Select size input
+let rows = document.getElementById('inputHeight').value;
+let columns = document.getElementById('inputWidth').value;
 
 // When size is submitted by the user, call makeGrid()
 
